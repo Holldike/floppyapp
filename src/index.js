7ef683eaaadd10893ecc4b6bea8c4c2ef7a90eb4
Vlad <ganchmix@gmail.com>
2021-03-02 23:30:56 +0200

    Gist is replased with chatra

--- src/index.js
+++ src/index.js
@@ -94,4 +94,17 @@ $(document).ready(function () {
 
     burgerMenu('.burger-menu');
     replaceContent('home');
+
+    //Chatra
+    (function(d, w, c) {
+        w.ChatraID = '8p86MwG7sfQzvrnai';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+    //Chatra
 })
